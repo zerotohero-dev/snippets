f4172aab69d18a277dffffe907244a23ca70d852
Volkan Özçelik <volkan.ozcelik@gmail.com>
2015-04-26 14:34:43 -0700

    Update dom.js

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,7 +1,3 @@
-function id(x) {
-    return document.getElementById(x);
-}
-
-function cls(x) {
-    return document.getElementsByClassName(x);
-}
+function id(x) {return document.getElementById(x);}
+function cls(x) {return document.getElementsByClassName(x);}
+function value(x) {return id(x).value;}
