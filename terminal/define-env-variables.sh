7bb04af69f37435ab055aa0c6bdf142551228f8d
Volkan Özçelik <volkan.ozcelik@gmail.com>
2015-05-03 21:43:42 -0700

    Create define-env-variables.sh

---
+++ terminal/define-env-variables.sh
@@ -0,0 +1,7 @@
+vi ~/.bash_proflle # to make the changes permanent.
+
+export ANT_HOME=/path/to/ant/dir
+export PATH=${PATH}:${ANT_HOME}/bin:${JAVA_HOME}/bin
+export DB2INSTANCE=prod_sales
+
+# etc, etc…
